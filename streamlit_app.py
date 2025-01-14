16cdd90464e6271c963d3a615bf98274dd31072c
Tom Willcox <108522086+WomTillcox@users.noreply.github.com>
2025-01-14 13:56:46 +0000

    changed write

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-
+import pandas as pd
 st.title("Octopus Call Dashboard")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Overview of key metrics"
 )
+
